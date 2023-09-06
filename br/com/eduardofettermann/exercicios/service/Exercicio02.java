9ea8448a6cf63f532513660f627506d889842172
Eduardo <eduardofettermann212@gmail.com>
2023-09-06 17:44:32 -0300

    nova feature: solucao exercicio-02 adicionada

---
+++ br/com/eduardofettermann/exercicios/service/Exercicio02.java
@@ -0,0 +1,7 @@
+package br.com.eduardofettermann.exercicios.service;
+
+public class Exercicio02 {
+    public void imprimeNumeros(int primeiroNumero, int segundoNumero){
+        System.out.println("Os números passados foram: " + primeiroNumero + " e " + segundoNumero);
+    }
+}
