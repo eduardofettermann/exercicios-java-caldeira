47108590231aef35ad484ae195859825f62ff882
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-09-18 00:02:38 -0300

    adicionando base conta

---
+++ br/com/eduardofettermann/exercicios/solucoes/lista3/Exercicio01Lista3/ContaBancaria.java
@@ -0,0 +1,9 @@
+package br.com.eduardofettermann.exercicios.solucoes.lista3.Exercicio01Lista3;
+
+public class ContaBancaria {
+    private String nome;
+    private String cpf;
+    private int id;
+    private String banco;
+    private double saldo;
+}
