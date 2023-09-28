4fd1ac7711bc408f66a37e400aba9ff7a952b3fa
Eduardo Fettermann <EduardoFettermann@MacBook-Pro-de-Administrador-2.local>
2023-09-28 16:06:29 -0300

    feat(solucaoes): adiciona solucao exercicio01-aula06

---
+++ br/com/eduardofettermann/exercicios/solucoes/aula07/solucao01aula07/model/Drawable.java
@@ -0,0 +1,8 @@
+package br.com.eduardofettermann.exercicios.solucoes.aula07.solucao01aula07.model;
+
+import br.com.eduardofettermann.exercicios.solucoes.aula07.solucao01aula07.service.Solucao01A07;
+
+public interface Drawable {
+    Solucao01A07 solution = new Solucao01A07();
+    void draw();
+}
